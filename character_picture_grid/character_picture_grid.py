156cafec0e3f01c1db402d8a02c3ef82f6aa8964
mrashleyball <me@mrashleyball.com>
2022-06-20 22:47:08 +1000

    Character Picture Grid added

--- character_picture_grid/character_picture_grid.py
+++ character_picture_grid/character_picture_grid.py
@@ -1,1 +1,35 @@
 # Character Picture Grid
+
+# Take grid
+# Loop through
+# Chage order of output [x][y]
+#   [8][0] goes first
+#   Loop through indexes
+# Print out new result
+
+
+grid = [['.', '.', '.', '.', '.', '.'], 
+        ['.', 'O', 'O', '.', '.', '.'], 
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def main(): draw(grid)
+
+def draw(l):
+    column, row = 8, 0 # Start in bottom left of grid list
+    for i in l: # Loop through lists in grid
+        for j in l: # Loop through items in each list
+            print(l[column][row], end='') # Print out as per starting point
+            column -= 1 # Reverse up the column, results in row output
+        if row < 5: # Go until row 6 (0-5)
+            column = 8 # Reset column back to the bottom or last
+            row += 1 # Move up a row to being new row
+            print('') # Print new line
+        else: break # Once greater than 6th row, break and exit out
+
+if __name__ == '__main__': main()
