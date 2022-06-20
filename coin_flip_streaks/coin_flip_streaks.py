e26aa289ac798855e1c26a7eb4b6e54cb2276fdf
mrashleyball <me@mrashleyball.com>
2022-06-20 20:18:23 +1000

    coin

--- coin_flip_streaks/coin_flip_streaks.py
+++ coin_flip_streaks/coin_flip_streaks.py
@@ -5,6 +5,7 @@
 # Track result
 # Store result in a list
 # Check list for a streak
+#   If current toss is the same six times in a row, add that to list of streaks
 # Output streak
 
 from random import randint
@@ -16,6 +17,15 @@ def flip(number):
     for i in range(int(number)):
         coin = randint(0,1)
         coin_list.append(coin)
-    return coin_list
+    return streak(coin_list)
+
+def streak(l):
+    streak_total = 0
+    for i in l:
+        if l[i] == l[i]
 
 if __name__ == '__main__': main()
+
+# Credits
+
+# https://www.reddit.com/r/learnpython/comments/6m8o4d/if_i_have_a_list_of_numbers_how_do_i_get_the/
