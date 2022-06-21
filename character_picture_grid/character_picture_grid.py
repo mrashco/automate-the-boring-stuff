81d5587b2e3c199c6c10b5578d72438d2a3e6a86
mrashleyball <me@mrashleyball.com>
2022-06-21 16:49:56 +1000

    Updates to Character Code

--- character_picture_grid/character_picture_grid.py
+++ character_picture_grid/character_picture_grid.py
@@ -21,14 +21,14 @@ grid = [['.', '.', '.', '.', '.', '.'],
 def main(): draw(grid)
 
 def draw(l):
-    column, row = 8, 0 # Start in bottom left of grid list
+    row, column = 8, 0 # Start in bottom left of grid list
     for i in l: # Loop through lists in grid
         for j in l: # Loop through items in each list
-            print(l[column][row], end='') # Print out as per starting point
-            column -= 1 # Reverse up the column, results in row output
-        if row < 5: # Go until row 6 (0-5)
-            column = 8 # Reset column back to the bottom or last
-            row += 1 # Move up a row to being new row
+            print(l[row][column], end='') # Print out as per starting point
+            row -= 1 # Reverse up the column, results in row output
+        if column < 5: # Go until row 6 (0-5)
+            row = 8 # Reset column back to the bottom or last
+            column += 1 # Move up a row to being new row
             print('') # Print new line
         else: break # Once greater than 6th row, break and exit out
 
